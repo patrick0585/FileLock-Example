5282667e66fc5e0a3661c2da25499632df58dedf
Patrick0585 <patrick0585@googlemail.com>
2022-06-20 21:32:19 +0200

    Formatted with clang-format 'google-style'

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/file.h>
 #include <math.h>
 #include <string.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <iostream>
 
 #define BALANCE "balance.dat"
 #define LOCKFILE "balance.lock"
@@ -13,82 +14,79 @@
  *	Returns the size of a file
  */
 int getFileSize() {
-	struct stat fileInfos;
-	stat(BALANCE, &fileInfos);
-	int fileSize = fileInfos.st_size;
-	return fileSize;
+  struct stat fileInfos;
+  stat(BALANCE, &fileInfos);
+  int fileSize = fileInfos.st_size;
+  return fileSize;
 }
 
 /*
  *	Reads the balance from the file
  */
 int getBalance() {
+  int fileSize = getFileSize();
+  size_t bytesRead = 0;
+  int balance = 0;
 
-	int fileSize = getFileSize();
-	size_t bytesRead = 0;
-	int balance = 0;
-	
-	int fd = open(BALANCE, O_RDONLY);
-	if ( fd == -1 ) {
-		fprintf(stderr, "Cannot open file '%s'!\n",BALANCE);
-		exit(1);
-	}
+  int fd = open(BALANCE, O_RDONLY);
+  if (fd == -1) {
+    fprintf(stderr, "Cannot open file '%s'!\n", BALANCE);
+    exit(1);
+  }
 
-	char *buffer = new char[fileSize + 1];	
-	bytesRead = read(fd, buffer, fileSize);
-	buffer[fileSize] = '\0';
-	sscanf(buffer, "%d", &balance);
-	close(fd);
+  char *buffer = new char[fileSize + 1];
+  bytesRead = read(fd, buffer, fileSize);
+  buffer[fileSize] = '\0';
+  sscanf(buffer, "%d", &balance);
+  close(fd);
 
-	printf("The current balance is %d$\n",balance);
+  printf("The current balance is %d$\n", balance);
 
-	return balance;
+  return balance;
 }
 
 /*
  *	Updates the balance in the file
  */
 ssize_t writeBalance(int balance) {
+  int fd = open(BALANCE, O_WRONLY | O_CREAT);
+  if (fd == -1) {
+    fprintf(stderr, "Cannot open file '%s'!\n", BALANCE);
+    exit(1);
+  }
 
-	int fd = open(BALANCE, O_WRONLY | O_CREAT);
-	if ( fd == -1 ) {
-		fprintf(stderr, "Cannot open file '%s'!\n",BALANCE);
-		exit(1);
-	}
+  // The number of digits of the credit amount
+  int digits = floor(log10(abs(balance))) + 1;
 
-	// The number of digits of the credit amount
-	int digits = floor(log10(abs(balance)))+1;
+  char b1[digits];
+  sprintf(b1, "%d", balance);
+  b1[digits] = '\0';
+  char *p = b1;
 
-	char b1[digits];
-	sprintf(b1, "%d", balance);
-	b1[digits] = '\0';
-	char *p = b1;
-	
-	printf("The balance was increased to %d$\n", balance);
+  printf("The balance was increased to %d$\n", balance);
 
-	ssize_t writtenBytes = write(fd, p, sizeof(b1));
-    
-	return writtenBytes;
+  ssize_t writtenBytes = write(fd, p, sizeof(b1));
+
+  return writtenBytes;
 }
 
 int main(int argc, char *argv[]) {
-
 #ifdef LOCK
-	int lockFd = open(LOCKFILE, O_RDWR | O_CREAT, 0666);
-	if ( flock(lockFd, LOCK_EX ) == -1 ) {
-        std::cout << "ERROR\n";
-    }
+  int lockFd = open(LOCKFILE, O_RDWR | O_CREAT, 0666);
+  if (flock(lockFd, LOCK_EX) == -1) {
+    std::cout << "ERROR\n";
+  }
 #endif
 
-	for(int i=0; i<=3; i++) {
-		int balance = getBalance();	
-		balance += 20;
-		writeBalance(balance);
-        sleep(2);
-    }
+  for (int i = 0; i <= 3; i++) {
+    int balance = getBalance();
+    balance += 20;
+    writeBalance(balance);
+    sleep(2);
+  }
 
 #ifdef LOCK
-	unlink(LOCKFILE);
-	flock(lockFd, LOCK_UN);
+  unlink(LOCKFILE);
+  flock(lockFd, LOCK_UN);
 #endif
 }
